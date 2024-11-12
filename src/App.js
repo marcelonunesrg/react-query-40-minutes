b42b86c85f6b66426501121d2b21f1eabd02f30b
Marcelo Nunes <marcelopereiranunes@gmail.com>
2024-11-12 16:45:24 -0500

    Example updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// 'https://jasonplaceholder.typicode.com/todos';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const queryClient = useQueryClient();
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      return response.json();
+    },
+    refetchInterval: 30 * 1000,
+    refetchOnWindowFocus: false,
+    retry: 3,
+  });
+
+  const { mutate, isPending, isError, isSuccess } = useMutation({
+    mutationFn: async (newPost) => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify(newPost),
+      });
+
+      return response.json();
+    },
+    onSuccess: (newPost) => {
+      // queryClient.invalidateQueries({
+      //   queryKey: ['posts'],
+      // });
+      queryClient.setQueryData(['posts'], (oldPosts) => [...oldPosts, newPost]);
+    },
+    // enabled: false,
+  });
+
+  // TODO: Explore useInfinityQuery for continuous lazy loading
+
+  if (error || isError) return <div>Error: {error.message}</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return <div className="App">
+    
+    {isPending && <div>Adding Post...</div>}
+
+    <button onClick={() => mutate({ 
+      userId: 3000,
+      title: 'Post #1', 
+      body: 'This is the first post',  
+    })}>
+      Add Post
+    </button>
+
+    {data && data.map((todo) => 
+      <div key={todo.id}>
+        <h4>ID: {todo.id}</h4>
+        <h4>TITLE: {todo.title}</h4>
+        <p>{todo.body}</p>
+      </div>)}
+  </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+      gcTime: 600 * 1000,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <QueryClientProvider client={queryClient}>
+        <App /> 
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
